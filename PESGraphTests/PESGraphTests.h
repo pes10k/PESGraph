a203f92884e0bba33b440cd44923456b7e824649
Luke Docksteader <luke@docksteaderluke.com>
2014-03-18 14:16:18 -0300

    Upgrading project for latest Xcode settings. Converting tests to use XCUnit Test Corrected all "implicit conversion" warnings.

--- PESGraphTests/PESGraphTests.h
+++ PESGraphTests/PESGraphTests.h
@@ -6,8 +6,8 @@
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
-#import <SenTestingKit/SenTestingKit.h>
+#import <XCTest/XCTest.h>
 
-@interface PESGraphTests : SenTestCase
+@interface PESGraphTests : XCTestCase
 
 @end
